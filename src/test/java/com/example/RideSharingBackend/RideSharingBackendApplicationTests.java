a43b49e909951a9ead5fc8f632351a4d7cf569ba
MichealSebre <michealsultan1234@gmail.com>
2020-11-13 17:48:47 -0500

    first commit

---
+++ src/test/java/com/example/RideSharingBackend/RideSharingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RideSharingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSharingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
