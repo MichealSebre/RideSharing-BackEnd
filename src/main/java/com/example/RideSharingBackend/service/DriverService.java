d328cc94a348d3dafd3e74e43236eee5d38c79ad
Micheal <michealsultan1234@gmail.com>
2021-01-12 17:05:41 -0500

    2nd base commit

---
+++ src/main/java/com/example/RideSharingBackend/service/DriverService.java
@@ -0,0 +1,39 @@
+package com.example.RideSharingBackend.service;
+
+import com.example.RideSharingBackend.dao.DriverRepository;
+import com.example.RideSharingBackend.entity.Driver;
+import com.example.RideSharingBackend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DriverService {
+    @Autowired
+    private DriverRepository repo;
+
+    public Driver saveDriver(Driver driver){
+        return repo.save(driver);
+    }
+
+    public List<Driver> getAllDriver(){
+        return repo.findAll();
+    }
+
+    public Driver getDriverById(Long id){
+        return repo.getOne(id);
+    }
+
+    public Optional<Driver> findById(Long id){
+        return repo.findById(id);
+    }
+
+
+
+    public void deleteDriver(Long id){
+        repo.deleteById(id);
+    }
+
+}
